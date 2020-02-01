1f06b0c671de2fd267109e44054fdfeed979d8b4
Matthew Starr <buylowsellhigh@me.com>
2020-02-01 05:03:34 -0500

    Not able to print burgers to browser

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Single Patty', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+
