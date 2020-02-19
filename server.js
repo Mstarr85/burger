7d649a23eaca6ac572cca700ace503468de7690b
Matthew Starr <buylowsellhigh@me.com>
2020-02-18 19:25:20 -0500

    added compression npm to app

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require("express");
 
+// added this for activity 18-3 
+var compression = require('compression')
+
+
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+// added this for activity 18-3 
+app.use(compression())
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
